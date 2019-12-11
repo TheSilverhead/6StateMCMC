be6e5317197c28fb5953f158908b1e7bcfd11607
TheSilverhead <jordan.john.weaver@gmail.com>
2019-12-11 14:48:18 -0500

    Staged MCMC sampling; added data logs
    MCMC was unstable with large (1e6) samples; added a dedicated burn-in stage and batches of samples with parameter, energy landscape and acceptance ratio logging.

--- JordanExample.jl
+++ JordanExample.jl
@@ -1,13 +1,14 @@
 using DifferentialEquations,ParameterizedFunctions, DiffEqParamEstim #DiffEq
 using RecursiveArrayTools, StatsBase,Distributions #Vector of Arrays and stats
 using StatsPlots, CSV, DataFrames,Printf,Dierckx,ProgressMeter
-
+using AverageShiftedHistograms, DelimitedFiles, Statistics
 ###########################
 #ToDo
 #Figure out how to deal with parameter boundaries
 #Include priors into MH MCMC?
 ###########################
-
+cd()
+cd(".\\Documents\\GitHub\\6StateMCMC\\")
 #Import the MCMC solver
 include("main.jl")
 #Import error function
@@ -16,96 +17,73 @@ include("LossFunctionJordan.jl")
 function Model!(dy,y,par,t)
   #IFN, ODE 1 parameters
   k11=0 #PR8, RIGI is assumed antagonized
-  k12=par[2]
   n=3
-  k13=par[3]
-  k14=par[4]
-  tau1=par[22]
+  RIGI=1
+  k12=par[1]
+  k13=par[2]
+  k14=par[3]
   #IFN_env, ODE 2 parameters
-  k21=par[5]
-  tau2=par[23]
-  #STAT, ODE 3 parameters
-  r31=par[6]
-  k31=par[7]
-  tau3=par[24]
-  #STATP, ODE 4 parameters
-  k41=par[8]
-  k42=par[9]
-  tau4=par[25]
-  #IRF7, ODE 5 parameters
-  k51=par[10]
-  k52=par[11]
-  tau5=par[26]
-  #IRF7P, ODE 6 parameters
+  k21=par[4]
+  tau2=par[5]
+  #STATP, ODE 3 parameters
+  k31=par[6]
+  k32=par[7]
+  k33=par[8]
+  #IRF7, ODE 4 parameters
+  k41=par[9]
+  k42=par[10]
+  #IRF7P, ODE 5 parameters
+  k51=par[11]
+  #Cells, ODE 6 parameters
   k61=par[12]
-  tau6=par[27]
-  #Target cells, ODE 7 paramters
+  #Virus, ODE 7 parameters
   k71=par[13]
-  #Eclipse infected cells, ODE 8 parameters
-  k81=par[14]
-  k82=par[15]
-  #Productive infected cells, ODE 9 parameters
-  k91=par[16]
-  #Viral count, ODE 10 parameters
-  k10_1=par[17]
-  k10_2=par[18]
-  k10_3=par[19]
-  #TJ Constants
-  #TJ describes the binding of IFN and SOCS feedback
-  TJtot=0.0001
-  k11_1=par[20]
-  k11_2=par[21]
-  TJ=TJtot*(y[2]/(k11_1+y[2])*(1/(1+k11_2))); #Eq. 11
+  k72=par[14]
 
+  V=y[7]
   #ODE System
-  v=y[10]/(10^4) #scale virus effects
-  i1=y[8]/(10^4) #scale eclipse cell effects
-  dy[1]=(k11*v)+(k12*(v^n))/(k13+(v^n))+k14*y[6]-y[1]*tau1 #IFN in cytoplasm
-  dy[2]=(k21*y[1])-(y[2]*tau2) #IFN in environment
-  dy[3]=r31+k31*y[4]-y[3]*tau3 #STAT in cytoplasm
-  dy[4]=(k41*TJ*y[3])/(k42+y[3])-y[4]*tau4 #STATP
-  dy[5]=k51*y[4]+k52*y[6]-y[5]*tau5 #IRF7
-  dy[6]=k61*y[5]-y[6]*tau6 #IRF7P
-  dy[7]=-k71*y[7]*y[10] #Uninfected target cells
-  dy[8]=k71*y[7]*y[10]-(k81*i1)/(1+k82*y[2]) #Eclipse infected cells
-  dy[9]=(k81*i1)/(1+k82*y[2])-k91*y[9] #Productive infected cells
-  dy[10]=(k10_1*y[9])/(1+k10_2*y[2])-k10_3*y[10] #Virus count
-
+  dy[1]=k11*RIGI*V+(k12*V^n)/(k13+V^n)+k14*y[5]-k21*y[1]
+  dy[2]=k21*y[1]-tau2*y[2]
+  dy[3]=(k31*y[2])/(k32+k33*y[2])-0.3*y[3]
+  dy[4]=k41*y[3]+k42*y[5]-0.3*y[4]
+  dy[5]=k51*y[4]-0.3*y[5]
+  dy[6]=-k61*y[6]*y[7]
+  dy[7]=(k71*y[6]*y[7])/(y[2]+y[7])-k72*y[7]
 end
 
 #parNames = ["a","b","c"]
 #parNum = length(parNames)
-stateNames = ["IFN","IFNenv","STAT1","STATP2n","IRF7","IRF7Pn","Target","Eclipse","Productive","Virus"]
-parNum = 27
+stateNames = ["IFN","IFNe","STATP","IRF7","IRF7P","Live Cells","Virus"]
+parNum =14
 
 #Define information for ODE model
-p=rand(parNum) #Parameter values
-u0 = [7.94, 0, 262.3, 12.2, 14.15, 0, 0, 250000, 0, 7.5E-2] #Initial Conditions
+#p=rand(parNum) #Parameter values
+p=[1e-3, 1e-3, 3.6e-5, 0.02, 0.2, 1, 1e-3, 1e-3, 3.6e-4, 3.2e-7, 360, 0.01, 1000, 0.001]
+u0 = [7.939415, 0, 12.2075, 14.14915, 0.001, 1, 6.9e-8] #Initial Conditions
 tspan = (0.25,24.0) #Time (start, end)
 
 #Contruct the ODE Problem
 prob = ODEProblem(Model!,u0,tspan,p)
-alg = Vern7()  #ODE solver
+alg = Vern7()   #ODE solver
 
 #Solve for the "True" solution
 sol = solve(prob,alg)
 
 ## Generate data
 
-data = CSV.read("C:/Users/Portable/Documents/GitHub/6StateMCMC/PR8.csv")
+data = CSV.read("PR8.csv")
 t = convert(Array,data[:Time])
-mock = CSV.read("C:/Users/Portable/Documents/GitHub/6StateMCMC/Control.csv")
-ci = CSV.read("C:/Users/Portable/Documents/GitHub/6StateMCMC/PR8CI95.csv")
-titer = CSV.read("C:/Users/Portable/Documents/GitHub/6StateMCMC/ViralTiters.csv")
+mock = CSV.read("Control.csv")
+titer = CSV.read("ViralTiters.csv")
 t_titer=convert(Array,titer[:Time])
 
-removeCol = [:Time :IFN_env] #Strip time and IFN_env from data
+removeCol = [:Experiment :Time :IFN_env] #Strip time and IFN_env from data
 for col in removeCol
   deletecols!(data,col)
   deletecols!(mock,col)
 end
 
-removeCol = [:Time :dNS1PR8] #Strip time from viral titers
+removeCol = [:Experiment :Time :dNS1PR8] #Strip time from viral titers
 for col in removeCol
   deletecols!(titer,col)
 end
@@ -113,36 +91,62 @@ end
 data = convert(Matrix,data)
 mock = convert(Matrix,mock)
 titer = convert(Matrix,titer)
-ci = convert(Matrix,titer)
 
 #Data points being measured
-measured = [1,3,4,5]
+measured = [1,3,4]
 
 ## Supply prior distributions
 priors = fill(Uniform(0,1), parNum)
 parBounds = fill([0.0, Inf], parNum) #Fill bounds with 0/inf
-parBounds[14]=Float64[0.0, 1.0]; #Modify bounds with known values
-parBounds[16]=Float64[0.0, 1.0];
-parBounds[19]=Float64[0.0, 1.0];
+#Modify bounds with known values
+parBounds[5]=Float64[0.0, 1.0]
+parBounds[12]=Float64[0.0, 0.1]
+parBounds[13]=Float64[0.0, 1e12]
+parBounds[14]=Float64[0.0, 0.1]
 
-lossFunc = LossLog(t,t_titer,data,measured,mock,ci,titer)
+lossFunc = LossLog(t,t_titer,data,measured,mock,titer)
 
-sampleNum = Int(1e6)
-result = ptMCMC(prob,alg,priors,parBounds,lossFunc,sampleNum)
+burnSamples=Int(1e3)
+stageNum=Int(1e3)
+sampleNum = Int(1e3)
 
+#Run a burn-in
+result = ptMCMC(prob,alg,priors,parBounds,lossFunc,burnSamples)
+bestPars= dropdims(permutedims(result[1], [1, 3, 2])[argmax(result[2],dims=1),:],dims=1)
+pNew=bestPars[1,:]
+global probNew = remake(prob, p=pNew) #Remake with burnt parameters
+
+for i=1:stageNum #Run batches of sampleNum MCMC samples, stageNum times
+  result = ptMCMC(probNew,alg,priors,parBounds,lossFunc,sampleNum)
+  bestPars= dropdims(permutedims(result[1], [1, 3, 2])[argmax(result[2],dims=1),:],dims=1)
+  pNew=bestPars[1,:]
+  global probNew = remake(prob, p=pNew)
+
+  open("chains.csv","a") do io #Write out current parameter set
+    writedlm(io,bestPars[1,:])
+  end
+  open("acceptRatio.csv","a") do io #Write out acceptance ratio
+    writedlm(io,mean(result[3][:,1]))
+  end
+open("energy.csv","a") do io #Write out loss function value
+  writedlm(io,mean(result[2][:,1]))
+end
+
+end
+
+#Pull best results from the best chain
 bestPars= dropdims(permutedims(result[1], [1, 3, 2])[argmax(result[2],dims=1),:],dims=1)
 bestChains = [remake(prob;p=bestPars[i,:]) for i=1:size(bestPars,1)]
 chainSols = [solve(problem,alg) for problem in bestChains]
 
 ###############
-cd("C:/Users/Portable/Documents/GitHub/6StateMCMC")
 ODEplots = Vector(undef,length(u0))
 
 mockSplines = [Spline1D(t,mock[:,i];k=1) for i=1:size(mock,2)]
 
 plotRecipe = (t,x,i,j) -> plot(t,x,label="Chain $j",title=stateNames[i],framestyle=:box,yformatter=y->"$(@sprintf("%.0e",y))",legend=false)
 
-Lfc(c,m) = @. log2(c+1) - log2(m+1)
+Lfc(c,m) = @. log2(c) - log2(m)
 #loop through states
 for i in eachindex(u0)
 
@@ -151,7 +155,7 @@ for i in eachindex(u0)
       midx = findfirst(x->x==i,measured)
       logScaledSim = Lfc(chainSols[1][i,:], mockSplines[midx](chainSols[1].t))
       ODEplots[i] = plotRecipe(chainSols[1].t,logScaledSim,i,1)
-      scatter!(t,data[:,findfirst(x->x==i,measured)],label="Data: "*stateNames[i],layout=(2,3))
+      scatter!(t,data[:,findfirst(x->x==i,measured)],label="Data: "*stateNames[i])
   else
       ODEplots[i] = plotRecipe(chainSols[1].t,chainSols[1][i,:],i,1)
   end
@@ -169,17 +173,13 @@ for i in eachindex(u0)
 
 end
 
-
-datafitPlot = plot(ODEplots...,layout=(2,5),size=(1000,600))
+datafitPlot = plot(ODEplots...,size=(1920,1080))
 
 savefig(datafitPlot,"JordanDataFit.pdf")
 
-
-using AverageShiftedHistograms
-
 #Posterior
 HistData = [plot(ash(result[1][1:end,i,1]),title="Par $i") for i=1:parNum]
-histPlot=plot(HistData...,legend=false,size=(2000,1200))
+histPlot=plot(HistData...,legend=false,size=(1920,1080))
 
 savefig(histPlot,"histPlot.pdf")
 
--- LossFunctionJordan.jl
+++ LossFunctionJordan.jl
@@ -1,10 +1,9 @@
-struct LossLog{time,time_titer,data,M,control,ci,titer} <: DiffEqBase.DECostFunction
+struct LossLog{time,time_titer,data,M,control,titer} <: DiffEqBase.DECostFunction
   t::time #Time points for data
   t_titer::time_titer #Time points for virus count
   data::data # LFC of data
   measured::M #Indices in model with data
   mock::control #raw data for control
-  interval::ci #95% Confidence intervals
   vtiter::titer #Viral titer estimates
 end
 
@@ -22,7 +21,6 @@ t = f.t
 data = f.data
 measured = f.measured
 mock = f.mock
-interval = f.interval
 vtiter = f.vtiter
 
   if sol.retcode != :Success
@@ -35,23 +33,14 @@ vtiter = f.vtiter
   maxlog2(x) = log2(max(0,x)+1)
   @inbounds for i = 1:length(subSol) #Loop through each data time point
     for (j,m) in enumerate(measured) #loop though states
-      foo=(maxlog2(subSol[m,i]) - maxlog2(mock[i,j]))
-      refmax = data[i,j]+interval[i,j]*0
-      refmin = data[i,j]-interval[i,j]*0
       sumsq +=(maxlog2(subSol[m,i]) - maxlog2(mock[i,j]) - data[i,j])^2
-      if (foo>refmax)
-        sumsq +=(foo-refmax)^2 #Above CI error
-      elseif (foo<refmin)
-        sumsq +=(foo-refmin)^2 #Below CI error
-      end
-
     end
   end
 
 subSol = sol(t_titer)
 
 @inbounds for i = 1:length(subSol) #Loop through each virus time point
-  sumsq += 10*(log10(titer[i,1])-log10(subSol[10,i]))^2 #Virus ssqe, weighted
+  sumsq += 100*(titer[i,1]-subSol[7,i])^2 #Virus ssqe, weighted
 end
 
 return sumsq
