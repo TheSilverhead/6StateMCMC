6dc2ad41afb011bc922aaaba103fd3ed29d1b437
TheSilverhead <jordan.john.weaver@gmail.com>
2019-12-16 10:55:11 -0500

    Corrected for microarray background noise
    ODE's now start at proper initial conditions (0 at time 0), then add a microarray background reading correction before taking the log-fold change.

--- JordanExample.jl
+++ JordanExample.jl
@@ -59,9 +59,10 @@ parNum =14
 #Define information for ODE model
 #p=rand(parNum) #Parameter values
 p=[1e-3, 1e-3, 3.6e-5, 0.02, 0.2, 1, 1e-3, 1e-3, 3.6e-4, 3.2e-7, 360, 0.01, 1000, 0.001]
-u0 = [7.939415, 0, 12.2075, 14.14915, 0.001, 1, 6.9e-8] #Initial Conditions
+#u0 = [7.939415, 0, 12.2075, 14.14915, 0.001, 1, 6.9e-8] #Initial Conditions
+u0 = [0, 0, 0, 0.72205, 0, 1, 6.9e-8] #Shifted Initial Conditions
 tspan = (0,24.0) #Time (start, end)
-
+shift= [7.939, 0, 12.2075, 13.4271, 0, 0, 0] #Noise shift on data
 #Contruct the ODE Problem
 prob = ODEProblem(Model!,u0,tspan,p)
 alg = Vern7()   #ODE solver
@@ -103,11 +104,11 @@ parBounds[5]=Float64[0.0, 1.0]
 parBounds[12]=Float64[0.0, 0.1]
 parBounds[14]=Float64[0.0, 0.1]
 
-lossFunc = LossLog(t,t_titer,data,measured,mock,titer)
+lossFunc = LossLog(t,t_titer,data,measured,mock,titer,shift)
 
 burnSamples=Int(1e1)
 stageNum=Int(1e1)
-sampleNum = Int(1e1)
+sampleNum = Int(1e2)
 
 #Run a burn-in
 result = ptMCMC(prob,alg,priors,parBounds,lossFunc,burnSamples)
@@ -146,13 +147,15 @@ mockSplines = [Spline1D(t,mock[:,i];k=1) for i=1:size(mock,2)]
 plotRecipe = (t,x,i,j) -> plot(t,x,label="Chain $j",title=stateNames[i],framestyle=:box,yformatter=y->"$(@sprintf("%.0e",y))",legend=false)
 
 Lfc(c,m) = @. log2(c) - log2(m)
+LfcShift(c,m,s) = @. log2(c+s) - log2(m)
 #loop through states
 for i in eachindex(u0)
 
 #Make the first plot for the first chain
   if i ∈ measured
       midx = findfirst(x->x==i,measured)
-      logScaledSim = Lfc(chainSols[1][i,:], mockSplines[midx](chainSols[1].t))
+      #logScaledSim = Lfc(chainSols[1][i,:], mockSplines[midx](chainSols[1].t))
+      logScaledSim = LfcShift(chainSols[1][i,:], mockSplines[midx](chainSols[1].t), shift[i])
       ODEplots[i] = plotRecipe(chainSols[1].t,logScaledSim,i,1)
       scatter!(t,data[:,findfirst(x->x==i,measured)],label="Data: "*stateNames[i])
   else
@@ -163,7 +166,8 @@ for i in eachindex(u0)
   for j=2:length(chainSols)
     if i ∈ measured
       midx = findfirst(x->x==i,measured)
-      logScaledSim = Lfc(chainSols[j][i,:], mockSplines[midx](chainSols[j].t))
+      #logScaledSim = Lfc(chainSols[j][i,:], mockSplines[midx](chainSols[j].t))
+      logScaledSim = LfcShift(chainSols[j][i,:], mockSplines[midx](chainSols[j].t), shift[i])
       plot!(chainSols[j].t,logScaledSim,label="Chain $j")
     else
       plot!(chainSols[j].t,chainSols[j][i,:],label="Chain $j")
